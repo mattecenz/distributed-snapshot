7989414ae0838aa411e6d02331cf8e4cb593611c
matteo <matteo.briscini@mail.polimi.it>
2025-03-12 14:05:58 +0100

    serializator

---
+++ src/main/java/polimi/ds/dsnapshot/Utilities/SerializationUtils.java
@@ -0,0 +1,26 @@
+package polimi.ds.dsnapshot.Utilities;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class SerializationUtils {
+    public static <T extends Serializable> byte[] serialize(T object) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+             oos.writeObject(object);
+             return bos.toByteArray();
+        }
+    }
+    @SuppressWarnings("unchecked") //Unchecked cast: 'java. lang. Object' to 'T'
+    public static <T extends Serializable> T deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+             return (T) ois.readObject();
+        }
+    }
+
+}
