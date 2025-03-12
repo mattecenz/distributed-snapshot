8fdd71d3263e62c1ddb30e705d3e27f2c8a7ba0a
matteo <matteo.briscini@mail.polimi.it>
2025-03-12 13:32:45 +0100

    Create Event System

---
+++ src/main/java/polimi/ds/dsnapshot/Events/Event.java
@@ -0,0 +1,39 @@
+package polimi.ds.dsnapshot.Events;
+
+import polimi.ds.dsnapshot.Connection.Messages.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+//TODO: is necessary to sync?
+public class Event {
+    private final List<CallbackRunner> callbacks = new ArrayList<>();
+
+    public void subscribe(Consumer<Message> callback) {
+        callbacks.add(new CallbackRunner(callback));
+    }
+
+    public void unsubscribe(Consumer<Message> callback) {
+        callbacks.removeIf(runner -> Objects.equals(runner.callback, callback));
+    }
+
+    public void publish(Message message) {
+        for (CallbackRunner callback : callbacks) {
+            callback.call(message);
+        }
+    }
+
+    private static class CallbackRunner{
+        private Consumer<Message> callback;
+        public CallbackRunner(Consumer<Message> callback) {
+            this.callback = callback;
+        }
+
+        public void call(Message message) {
+            callback.accept(message);
+        }
+
+    }
+}
---
+++ src/test/java/polimi/ds/dsnapshot/Events/EventsBrokerTest.java
@@ -0,0 +1,58 @@
+package polimi.ds.dsnapshot.Events;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import polimi.ds.dsnapshot.Exception.EventException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class EventsBrokerTest {
+
+    @BeforeEach
+    public void setup() {
+        EventsBroker.removeAllEventsChannel();
+    }
+
+    @Test
+    public void createEvent() {
+        Event e1 = null, e2 = null;
+        assertDoesNotThrow(() -> EventsBroker.createEventChannel("e1"));
+        assertDoesNotThrow(() -> EventsBroker.createEventChannel("e2"));
+
+        assertDoesNotThrow(() ->EventsBroker.getEventChannel("e1"));
+        assertDoesNotThrow(() ->EventsBroker.getEventChannel("e2"));
+
+        try {
+            e1 = EventsBroker.getEventChannel("e1");
+            e2 = EventsBroker.getEventChannel("e2");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assert e1 != null;
+        assert e2 != null;
+        assert !e1.equals(e2);
+    }
+
+    @Test
+    public void testException(){
+        boolean exceptionThrown = false;
+
+        assertDoesNotThrow(() -> EventsBroker.createEventChannel("e1"));
+        try {
+            EventsBroker.createEventChannel("e1");
+        } catch (EventException e) {
+            exceptionThrown = true;
+        }
+
+        assert exceptionThrown;
+        exceptionThrown = false;
+
+        try {
+            EventsBroker.getEventChannel("e2");
+        } catch (EventException e) {
+            exceptionThrown = true;
+        }
+        assert exceptionThrown;
+    }
+}
